12f0d814080dbabcb7da34e92dce113085a29c4c
erin <erinverkoulen93@gmail.com>
2021-05-08 20:09:42 -0500

    commit tree 53b86190a9a59a4ea886e05373c7d7a972a85147

--- index.js
+++ index.js
@@ -1,23 +1,66 @@
-
-// Code your solutions in this file
-/*for (let age = 30; age < 40; age++){
+/*
+for(let age = 30; age < 40; age++) {
     console.log(`I'm ${age} years old. Happy birthday to me!`);
-
-const gifts = ["teddy bear", "drone", "doll"];
+    
+}
+*/
+const gifts = ['teddy bear', 'drone', 'doll'];
 
 function wrapGifts(gifts) {
     for(let i = 0; i < gifts.length; i++) {
         console.log(`Wrapped ${gifts[i]} and added a bow!`);
+        debugger;
     }
 
     return gifts;
-}
 
+}
 wrapGifts(gifts);
 
-//const stringNames = ['Lisa', 'Kaitlin', 'Jan'];
-//const eventName = 'surprise';
-*/
+
+function writeCards(stringNames, eventName) {
+    let message = [];
+    for (let i = 0; i < stringNames.length; i++) {
+        message.push(  `Thank you, ${stringNames[i]}, for the wonderful ${eventName} gift!`);
+    }
+    return message;
+}
+
+function countDown(integar) {
+    let i = integar;
+    while(i >= 0) {
+        console.log(i--);
+    }
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
 function writeCards(stringNames, event){
     let messages = [];
     debugger;
@@ -32,8 +75,6 @@ function writeCards(stringNames, event){
 
 }
 
-//writeCards(['Jane','Kaitlin', 'Tom'], 'birthday');
-//writeCards(names, event);
 
 function countDown(integar) {
     let i = integar;
@@ -41,3 +82,4 @@ function countDown(integar) {
         console.log(i--);
     }
 }
+*/
